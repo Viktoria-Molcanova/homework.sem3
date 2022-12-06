d4fc64b8c4c5a5ffe0e2a14b5098d4d1aec29c0b
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2022-12-06 22:10:37 +0700

    update program1

--- dz/Program1.cs
+++ dz/Program1.cs
@@ -2,5 +2,10 @@
 // трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите число ");
 int a = Convert.ToInt32(Console.ReadLine());
-a = (a / 10) % 10;
-Console.WriteLine(a);
+if (a>99 && a<1000)
+{
+    a = (a / 10) % 10;
+    Console.WriteLine(a);
+}
+else 
+Console.WriteLine("число не трёхзначное,повторите попытку");
